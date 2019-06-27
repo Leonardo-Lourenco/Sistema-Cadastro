e20c3c8aec1a78ebdcf934d22808c371bb236e30
Leo <lnddru@gmail.com>
2019-06-27 19:14:31 -0300

    Add files via upload

--- BD/TrabalhoAlexandre.sql
+++ BD/TrabalhoAlexandre.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 14-Abr-2019 às 13:36
+-- Generation Time: 23-Jun-2019 às 09:05
 -- Versão do servidor: 10.0.38-MariaDB-0ubuntu0.16.04.1
--- PHP Version: 7.0.33-0ubuntu0.16.04.3
+-- PHP Version: 7.0.33-0ubuntu0.16.04.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `TrabalhoAlexandre`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `curso`
+--
+
+CREATE TABLE `curso` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `nomeCurso` varchar(30) NOT NULL,
+  `cpf` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `curso`
+--
+
+INSERT INTO `curso` (`id`, `nome`, `email`, `nomeCurso`, `cpf`) VALUES
+(4, 'Thabata', 'carloscesa@gmail', 'Leonardo', '100.653.566-75');
 
 -- --------------------------------------------------------
 
@@ -44,6 +65,12 @@ INSERT INTO `login` (`id`, `nome`, `senha`) VALUES
 --
 
 --
+-- Indexes for table `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
@@ -53,6 +80,11 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `login`
 --
